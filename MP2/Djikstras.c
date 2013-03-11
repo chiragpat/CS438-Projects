3ffa8b78707c7c06a68db7fdb0377a250fc4d49f
Chirag Patel <chiragpat@gmail.com>
2013-03-11 13:09:08 -0500

    removed debug statements

--- MP2/Djikstras.c
+++ MP2/Djikstras.c
@@ -34,7 +34,6 @@ void build_hop_table(NodeGraph* graph) {
     graph->routes[num_routes].cost = node->distance;
     num_routes++;
 
-    printf("Off the heap %d:%d\n", node->node_number, node->distance);
     if (node->distance == INT_MAX) {
       break;
     }
@@ -44,10 +43,8 @@ void build_hop_table(NodeGraph* graph) {
       Link *link = (Link *) node->neighbour_links[j];
       
       if ( (link->cost != -1) && (contains(visited_nodes, num_routes, neigh_node) == -1) ) {
-        printf("%d:%d\n", node->node_number, neigh_node->node_number);
         int altdist = node->distance + link->cost;
         if (altdist < neigh_node->distance) {
-          printf("%d:%d\n", node->node_number, neigh_node->node_number);
           if (node->hop == NULL) {
             neigh_node->hop = (struct Node*) neigh_node;
           }
@@ -66,8 +63,7 @@ void build_hop_table(NodeGraph* graph) {
   while (num_unvisited_nodes != 0) {
     Node *node = get_and_remove_min(unvisited_nodes, num_unvisited_nodes);
     num_unvisited_nodes--;
-
-    printf("%d:%d\n", node->node_number, node->distance);
+    
     graph->routes[num_routes].destination_node = node;
     graph->routes[num_routes].next_hop = (Node *) node->hop;
     graph->routes[num_routes].cost = node->distance;
