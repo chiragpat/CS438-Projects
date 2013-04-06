901ab89c9d0af3c14e38fe50a722e07642c4561b
Chirag Patel <chiragpat@gmail.com>
2013-04-06 01:49:21 -0500

    init for mp3

---
+++ MP3/receiver.c
@@ -0,0 +1,11 @@
+
+int run_receiver(char* portno, char* filename){
+
+	/*
+	 * Your implementation should be self-contained in this file, no other source
+	 * or header file will be considered by our autograder.
+	 */
+
+	return 0;
+}
+
---
+++ MP3/sender.c
@@ -0,0 +1,13 @@
+
+
+int run_sender(char* hostname, char *portno, char* filename) {
+
+	/*
+	 * Your implementation should be self-contained in this file, no other source
+	 * or header file will be considered by our autograder.
+	 */
+
+	return 0;
+}
+
+
