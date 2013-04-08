a2d49832325099b8fce49ec722713f923073f6b7
Chirag Patel <chiragpat@gmail.com>
2013-04-08 11:56:33 -0500

    added new subversion stuff

---
+++ MP3/mp3common.c
@@ -0,0 +1,8 @@
+/**
+ * CS438 - Spring 2013 - MP3 - mp3common.c
+ *
+ * Use this file for the functions that your receiver and sender will share
+ *
+ */
+
+#include "mp3common.h"
---
+++ MP3/mp3common.h
@@ -0,0 +1,13 @@
+/**
+ * CS438 - Spring 2013 - MP3 - mp3common.h
+ *
+ * Use this file for the functions that your receiver and sender will share
+ *
+ */
+
+
+#ifndef MP3COMMON_H_
+#define MP3COMMON_H_
+
+
+#endif
